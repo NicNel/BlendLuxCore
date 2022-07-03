dd137aa645026dd7e8323c82c6066331efe40af3
NicNel <100981393+NicNel@users.noreply.github.com>
2022-07-03 06:19:58 +0300

    Add colorspace parameters to the color of the matte material

--- nodes/materials/matte.py
+++ nodes/materials/matte.py
@@ -3,6 +3,7 @@ from bpy.props import FloatProperty
 from ..base import LuxCoreNodeMaterial
 from ..sockets import LuxCoreSocketFloat
 from ...utils import node as utils_node
+from ... import utils
 
 SIGMA_DESCRIPTION = "Surface roughness, 0 for pure Lambertian reflection"
 
@@ -33,5 +34,23 @@ class LuxCoreNodeMatMatte(bpy.types.Node, LuxCoreNodeMaterial):
         }
         if mat_type == "roughmatte":
             definitions["sigma"] = sigma
+            
+        #color space
+        if not self.inputs["Diffuse Color"].is_linked:
+            color = self.inputs["Diffuse Color"].export(exporter, depsgraph, props)
+            colorspace = bpy.context.scene.luxcore.config.colorspace
+            if colorspace == "opencolorio":
+                ocio_path = utils.get_abspath(bpy.context.scene.luxcore.config.ocio_conf_path)
+                colorspace_name = bpy.context.scene.luxcore.config.colorspace_default_name
+                definitions.update({
+                    "kd": [colorspace, ocio_path, colorspace_name, *color],
+                })
+            elif colorspace == "luxcore":
+                colorspace_gamma = bpy.context.scene.luxcore.config.colorspace_gamma
+                definitions.update({
+                    "kd": [colorspace, colorspace_gamma, *color],
+                })
+        #------------------
+        
         self.export_common_inputs(exporter, depsgraph, props, definitions)
         return self.create_props(props, definitions, luxcore_name)
